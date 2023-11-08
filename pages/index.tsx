a0545cb420b448e3ad7a5ebb8a1ddfcae78d3610
Karunakar Patel <karunakarpatel.kp@gmail.com>
2023-11-08 10:22:51 +0530

    Updated Same Code

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,17 +15,18 @@ import seo from "@Public/seo.jpg";
 
 import { ReactElement } from "react";
 import { NextPageWithLayout } from "./_app";
+import { SEO_OBJ, blogPostsObj } from "Essentials";
 
 const Home: NextPageWithLayout = () => {
   return (
     <>
       <HeaderSection
-        title="Vehicle Page"
-        description="This is the Description of the Vehicle Home Page"
-        image="https://image-url"
-        url="https://blog-page-url"
-        publishedTime="2023-01-07T21:58:00+00:00"
-        lastUpdatedTime="2023-01-07T21:58:00+00:00"
+        title={SEO_OBJ.HOME_PAGE.title}
+        description={SEO_OBJ.HOME_PAGE.description!}
+        image="https://raw.githubusercontent.com/karunakarpatel-kp/Blogging-2022/main/public/nextjs_install.png"
+        url={SEO_OBJ.HOME_PAGE.absoluteURL}
+        publishedTime={SEO_OBJ.HOME_PAGE.publishedTime}
+        lastUpdatedTime={SEO_OBJ.HOME_PAGE.lastUpdateTime}
       />
 
       <Box textAlign="center" mb={3}>
@@ -33,6 +34,23 @@ const Home: NextPageWithLayout = () => {
       </Box>
 
       <Grid container sx={{ spacing: { xs: 0, sm: 2, md: 2, lg: 2 }, pl: { xs: 1 } }}>
+        {blogPostsObj.map((singlePostObj) => {
+          return (
+            <Grid item xs={12} sm={12} md={4} lg={4} key={singlePostObj.id}>
+              <ImageOnTopCard
+                Title={singlePostObj.title}
+                Description={singlePostObj.description!}
+                href={singlePostObj.url}
+                ImageSrc={singlePostObj.featuredImage}
+                ImageAlt={singlePostObj.featuredImageAltText}
+                tags={singlePostObj.tags}
+              />
+            </Grid>
+          );
+        })}
+      </Grid>
+
+      {/* <Grid container sx={{ spacing: { xs: 0, sm: 2, md: 2, lg: 2 }, pl: { xs: 1 } }}>
         <Grid item xs={12} sm={12} md={4} lg={4}>
           <ImageOnTopCard
             Title="Learn More About Tata Safari Facelift."
@@ -47,35 +65,7 @@ const Home: NextPageWithLayout = () => {
             ]}
           />
         </Grid>
-        {/* <Grid item xs={12} sm={12} md={4} lg={4}>
-          <ImageOnTopCard
-            Title="Learn SEO Right From Start To Finish Image On Right Side"
-            Description="In this blog post you will be able o learn all the required technologies and most often you will able to see all the new era of technologies in this this blog post you will be able o learn all the required technologies and most often you will able to see all the new era of technologies in this World this blog post you will be able o learn all the required technologies and most often you will able to see all the new era of technologies in this World World.....!"
-            href="/Blog/seo"
-            ImageSrc={coding}
-            ImageAlt="Coding-Image"
-            tags={[
-              { tag: "seo", href: "/Blog/seo" },
-              { tag: "material ui", href: "/mui" },
-              { tag: "material ui", href: "/mui" },
-            ]}
-          />
-        </Grid>
-        <Grid item xs={12} sm={12} md={4} lg={4}>
-          <ImageOnTopCard
-            Title="Learn SEO Right From Start To Finish Image On Right Side"
-            Description="In this blog post you will be able o learn all the required technologies and most often you will able to see all the new era of technologies in this this blog post you will be able o learn all the required technologies and most often you will able to see all the new era of technologies in this World this blog post you will be able o learn all the required technologies and most often you will able to see all the new era of technologies in this World World.....!"
-            href="/Blog/seo"
-            ImageSrc={coding}
-            ImageAlt="Coding-Image"
-            tags={[
-              { tag: "seo", href: "/Blog/seo" },
-              { tag: "material ui", href: "/mui" },
-              { tag: "material ui", href: "/mui" },
-            ]}
-          />
-        </Grid> */}
-      </Grid>
+      </Grid> */}
 
       <Box textAlign="center" mb={6} sx={{ mt: { xs: 2, sm: 2, md: 4, lg: 6 }, mb: { xs: 2, sm: 4, md: 4, lg: 6 } }}>
         <Link
@@ -89,7 +79,7 @@ const Home: NextPageWithLayout = () => {
         >
           <Typography
             variant="h6"
-            sx={{ "&:hover": { textDecoration: "underline" }, color: "rgb(77, 79, 141)", textTransform: "none" }}
+            sx={{ "&:hover": { textDecoration: "underline" }, color: "#36106A", textTransform: "none" }}
           >
             Read more articles
           </Typography>
