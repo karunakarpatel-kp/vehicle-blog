f5ed8a683f65ff9af50e5f2ba858b2380caf3d68
Karunakar Patel <karunakarpatel.kp@gmail.com>
2023-12-09 21:51:43 +0530

    Fixed sitemap.xml creating again urls on page reload

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -3,31 +3,28 @@ import { SEO_OBJ } from "Essentials";
 
 const Sitemap = () => {};
 
-var sitemapURLOBJ: any = [];
-
-function pullURLAndLastUpdatedTime(incomingObj: any) {
-  Object.values(incomingObj).map((page: any) => {
-    sitemapURLOBJ.push({ url: page.absoluteURL, lastUpdatedTime: page.lastUpdateTime });
-  });
-  return sitemapURLOBJ;
-}
-
 export const getServerSideProps = ({ res }: any) => {
-  // Pull SEO_OBJ
+  // Initialize the array to store URLs
+  const sitemapURLOBJ: any = [];
+
+  function pullURLAndLastUpdatedTime(incomingObj: any) {
+    Object.values(incomingObj).map((page: any) => {
+      sitemapURLOBJ.push({ url: page.absoluteURL, lastUpdatedTime: page.lastUpdateTime });
+    });
+  }
+
+  // Pull URLs from SEO_OBJ
   pullURLAndLastUpdatedTime(SEO_OBJ);
 
-  //  Pull TataBlogPages
+  // Pull URLs from TataBlogPages
   pullURLAndLastUpdatedTime(SEO_OBJ.TATA_HOMEPAGE.TATA_PAGES_OBJ);
-
-  // console.log(sitemapURLOBJ, "@SITEMAP_URLS");
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
       <!-- We'll render the URLs for our sitemap here. -->
       ${sitemapURLOBJ
         .map((singleSiteData: any) => {
-          // console.log(singleSiteData, "SINGLESITEDATA");
-          return `<url> <loc>${singleSiteData.url}</loc>
+          return `<url><loc>${singleSiteData.url}</loc>
               <lastmod>${singleSiteData.lastUpdatedTime}</lastmod>
               <changefreq>weekly</changefreq>
               <priority>1.0</priority>
