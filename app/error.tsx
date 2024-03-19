ca171d30eaf3eb0c498a11317a535c5129b5c5f2
Karunakar Patel <karunakarpatel.kp@gmail.com>
2024-03-19 22:08:51 +0530

    Pushed sample for deployment

--- app/error.tsx
+++ app/error.tsx
@@ -6,6 +6,7 @@ const RootErrorPage = () => {
     <>
       <div className="h-full min-h-[69vh]">
         <p>This is the root Error Page in APP Folder</p>
+        <p>Pushed For Deployment Purpose bro...!</p>
       </div>
     </>
   );
