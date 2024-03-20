3cacb7287b81e7b62838b93de82953793a450893
Karunakar Patel <karunakarpatel.kp@gmail.com>
2024-03-20 15:47:25 +0530

    Fixed images after ; bug

--- components/MDXImage.tsx
+++ components/MDXImage.tsx
@@ -4,7 +4,7 @@ const MDXImage = (props: any) => {
   const { src, alt } = props;
   return (
     <div className=" relative h-full min-h-[200px] md:h-[300px] lg:h-[500px] bg-slate-200 rounded-lg ring-1 dark:bg-slate-800 ">
-      <Image src={src} alt={alt} layout="fill" />;
+      <Image src={src} alt={alt} layout="fill" />
     </div>
   );
 };
